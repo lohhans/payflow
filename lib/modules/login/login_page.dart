a069d4d5478ab80512d44005c5a7770aca5d3e03
Armstrong Lohãns <lohansdemelo1108@gmail.com>
2021-06-28 18:23:31 -0300

    module: firebase/login

--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/modules/login/login_controller.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_images.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
@@ -12,6 +13,8 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final controller = LoginController();
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -47,7 +50,7 @@ class _LoginPageState extends State<LoginPage> {
                   Image.asset(AppImages.logomini),
                   Padding(
                     padding:
-                    const EdgeInsets.only(top: 30, left: 70, right: 70),
+                        const EdgeInsets.only(top: 30, left: 70, right: 70),
                     child: Text(
                       "Organize seus boletos em um só lugar",
                       textAlign: TextAlign.center,
@@ -56,11 +59,9 @@ class _LoginPageState extends State<LoginPage> {
                   ),
                   Padding(
                     padding:
-                    const EdgeInsets.only(left: 40, right: 40, top: 40),
+                        const EdgeInsets.only(left: 40, right: 40, top: 40),
                     child: SocialLoginButton(
-                      onTap: () {
-                        print("clicou");
-                      },
+                      onTap: controller.googleSignIn,
                     ),
                   )
                 ],
